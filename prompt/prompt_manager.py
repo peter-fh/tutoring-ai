a756a7603ca4e93f676ca92c7fa63c8b8d5c8273
Disputatio <p.felty.howe@gmail.com>
2024-09-14 12:16:32 -0400

    Refactor prompt directory

--- prompt/prompt_manager.py
+++ prompt/prompt_manager.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-prompt_dir = "prompt/dynamic"
+prompt_dir = "prompt"
 example_response_file = prompt_dir + os.sep + "example_response.txt"
 prompt_text_file = prompt_dir + os.sep + "prompt.txt"
 course_json_file = prompt_dir + os.sep + "courses.json"
@@ -34,24 +34,7 @@ def generatePrompt(course_code: str, example_response=False):
 
     return return_prompt
 
-def getPrompt(course_code: str):
-    # Open the coures json file and extract the course specific information
-    course_file = open("courses.json")
-    courses = json.load(course_file)
-    course = courses[course_code]
-
-    # Open and read the prompt txt file
-    prompt = open("prompt.txt").read()
-
-    # Replace the placeholder variables with the course information
-    return_prompt = (prompt
-        .replace("{$name}", course["name"])
-        .replace("{$concepts}", course["content"])
-        .replace("{$prerequisite-concepts}", course["prerequisite"]))
-
-    # Return the final constructed prompt
-    return return_prompt
 
 # [Debug] Get example prompt and display it on the terminal
 if __name__ == "__main__":
-    print(getPrompt("MATH 209"))
+    print(generatePrompt("MATH 209"))
