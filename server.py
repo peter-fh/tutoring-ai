727c6dfb848242e536220550fb637ef8542deb5e
peter-fh <p.felty.howe@gmail.com>
2024-12-25 21:01:03 -0500

    Remove api abilities from redirect

--- server.py
+++ server.py
@@ -17,40 +17,6 @@ def index():
     return render_template("index.html")
 
 
-# Handles clicking the "Ask" button
-@app.route('/question/', methods=['POST'])
-def question():
-
-    if request.method == 'POST':
-
-        # If the question the student asks is more than a certain number of characters, reject the question
-        # Each character/word of the question contributes to the cost of the API call
-        # if len(request.data) > 2500:
-            # return "Message too long!"
-
-        # Get course type
-        course = request.headers["Course"]
-
-        # Get brevity level
-        brevity = request.headers["Brevity"]
-
-        # Get question type
-        question = request.headers["Type"]
-        prompt_type = PromptType[question.upper()]
-
-        # Get the question that the user asked from the HTTP request
-        message = request.get_json()
-
-        # Generate the prompt based on the course
-        course_info, prompt = generatePrompt(prompt_type, course, brevity)
-
-        # Ask the question with the context of the selected course
-        stream = ask(message, course_info, prompt, dummy_response=False) 
-
-        return Response(stream_with_context(stream), content_type="text/plain")
-
-    # Reject non-POST requests (flask should handle this regardless)
-    return "Invalid request type", 400
 
 
 # Run the server if this file is run
