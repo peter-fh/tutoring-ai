fbbde1eea780b24938b032d9dbcd083f88d7d12d
peter-fh <p.felty.howe@gmail.com>
2024-12-30 05:45:06 -0500

    chore(Server): Remove unnecessary error handling

--- server.py
+++ server.py
@@ -50,9 +50,6 @@ def index():
 
 @app.route('/summary', methods=['POST'])
 def summary():
-    if request.method != 'POST':
-        return "Invalid request type", 400
-
     conversation = request.get_json()
     return gpt.summarize(conversation)
 
@@ -76,9 +73,6 @@ def introduction():
 
 @app.route('/image', methods=['POST'])
 def image():
-    if request.method != 'POST':
-        return "Invalid request type", 400
-
     image = request.get_data(as_text=True)
     return gpt.transcribe(image)
 
@@ -86,40 +80,21 @@ def image():
 @app.route('/question', methods=['POST'])
 def question():
 
-    if request.method == 'POST':
+    # Retrieve question and its context from the request
+    course = request.headers["Course"]
+    brevity = request.headers["Brevity"]
+    question = request.headers["Type"]
+    prompt_type = PromptType[question.upper()]
+    conversation = request.get_json()
 
-        # If the question the student asks is more than a certain number of characters, reject the question
-        # Each character/word of the question contributes to the cost of the API call
-        # Disabled since a conversation after one message is already longer than this value
-        # TODO: add more protection/incentive against long conversation
+    # Generate the prompt based on the course
+    instructions = prompt(prompt_type, course, brevity)
 
-        # if len(request.data) > 2500:
-            # return "Message too long!"
+    # Ask the question with its context
+    stream = gpt.ask(conversation, instructions, prompt_type) 
 
-        # Get course type
-        course = request.headers["Course"]
+    return Response(stream_with_context(stream), content_type="text/plain")
 
-        # Get brevity level
-        brevity = request.headers["Brevity"]
-
-        # Get question type
-        question = request.headers["Type"]
-
-        prompt_type = PromptType[question.upper()]
-
-        conversation = request.get_json()
-        # Get the question that the user asked from the HTTP request
-
-        # Generate the prompt based on the course
-        instructions = prompt(prompt_type, course, brevity)
-
-        # Ask the question with the context of the selected course
-        stream = gpt.ask(conversation, instructions, prompt_type) 
-
-        return Response(stream_with_context(stream), content_type="text/plain")
-
-    # Reject non-POST requests (flask should handle this regardless)
-    return "Invalid request type", 400
 
 
 # Run the server if this file is run
